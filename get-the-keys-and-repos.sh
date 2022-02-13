cfe88a0244bc869ed46e40e39cdb3d1fc7e03155
Technopig100 <technopig@e.email.com>
2022-02-13 10:40:30 +0000

    Fix Adress

--- get-the-keys-and-repos.sh
+++ get-the-keys-and-repos.sh
@@ -6,14 +6,14 @@ sudo pacman -S wget --noconfirm --needed
 
 echo "Getting the ADK-Linux keys from the ADK-Linux repo"
 
-sudo wget https://github.com/Technopig100/adklinux_repo/tree/main/x86_64/adk-keyring-2.1-11-any.pkg.tar.zst -O /tmp/adk-keyring-2.1-11-any.pkg.tar.zst
+sudo wget https://github.com/Technopig100/adklinux_repo/raw/main/x86_64/adk-keyring-2.1-11-any.pkg.tar.zst -O /tmp/adk-keyring-2.1-11-any.pkg.tar.zst
 sudo pacman -U --noconfirm --needed /tmp/adk-keyring-2.1-11-any.pkg.tar.zst
 
 ######################################################################################################################
 
 echo "Getting the latest adklinux mirrors file"
 
-sudo wget https://github.com/Technopig100/adk-mirrorlist/blob/main/etc/pacman.d/adk-mirrorlist -O /etc/pacman.d/adklinux-mirrorlist
+sudo wget https://raw.githubusercontent.com/Technopig100/adk-mirrorlist/main/etc/pacman.d/adk-mirrorlist -O /etc/pacman.d/adklinux-mirrorlist
 echo '
 #ADK Linux repository
 
