b02e751607f9303e7722762039a6ca3d5d51736e
Technopig100 <technopig10@gmail.com>
2022-04-16 06:10:59 -0400

    install keys

--- get-the-keys-and-repos.sh
+++ get-the-keys-and-repos.sh
@@ -10,12 +10,12 @@ sudo pacman-key --lsign-key 0A74C5909A8A2600AD012AF67B22BF2F72D54DAC
 sudo pacman-key --recv-keys DB8C9336927412AD01764AD88C40B4EF4EF24C82 --keyserver hkps://keyserver.ubuntu.com 
 sudo pacman-key --lsign-key DB8C9336927412AD01764AD88C40B4EF4EF24C82 
 
-sudo pacman -Syy wget --noconfirm --needed
+sudo pacman -S wget --noconfirm --needed
 
 echo "Getting the ADK-Linux keys from the ADK-Linux repo"
 
-sudo wget https://github.com/Technopig100/adklinux_repo/raw/main/x86_64/adklinux-keyring-git* -O /tmp/adklinux-keyring-git*
-sudo pacman -U --noconfirm --needed /tmp/adklinux-keyring-git*
+#sudo wget https://github.com/Technopig100/adklinux_repo/raw/main/x86_64/adklinux-keyring-git* -O /tmp/adklinux-keyring-git*
+#sudo pacman -U --noconfirm --needed /tmp/adklinux-keyring-git*
 
 ######################################################################################################################
 
@@ -30,3 +30,6 @@ echo '
 
 [adklinux_repo]
 Include = /etc/pacman.d/adklinux-mirrorlist' | sudo tee --append /etc/pacman.conf
+
+sudo pacman -Syy adklinux-keyring-git --noconfirm --needed
+
